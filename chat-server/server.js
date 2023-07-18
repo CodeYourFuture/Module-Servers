43f65eab90ea8c207d0c0e4cbc01bdb3962c164e
Delnia Alipour <delniaalipur@gmail.com>
2023-07-18 15:46:55 +0100

    level 1 done

--- chat-server/server.js
+++ chat-server/server.js
@@ -3,8 +3,9 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
+const port = 4000;
 app.use(cors());
+app.use(express.json());
 
 const welcomeMessage = {
   id: 0,
@@ -20,7 +21,45 @@ const messages = [welcomeMessage];
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
+//create a new message
+app.post("/messages", function (request, response) {
+  let newMessage = request.body;
+  messages.push(newMessage);
+  console.log(
+    `New message has been added ==> id: ${newMessage.id} from: ${newMessage.from} text: ${newMessage.text}`
+  );
+  response.json(newMessage);
+});
 
-app.listen(process.env.PORT,() => {
-  console.log(`listening on PORT ${process.env.PORT}...`);
+//read all messages
+app.get("/messages", function (request, response) {
+  response.json(messages);
+});
+
+//Read one message specified by an ID
+app.get("/messages/:id", function (request, response) {
+  let idToFind = request.params.id;
+  const messageToFind = messages.find((el) => el.id == parseInt(idToFind));
+  if (messageToFind) {
+    response.json(messageToFind);
+  } else {
+    response.json(`Message with id ${idToFind} not found!`);
+  }
+});
+
+// Delete a message, by ID
+app.delete("/messages/:id", function (request, response) {
+  let messageIdToDelete = request.params.id;
+  let messageIndex = messages.findIndex(
+    (el) => el.id == parseInt(messageIdToDelete)
+  );
+  if (messageIndex !== -1) {
+    messages.splice(messageIndex, 1);
+    response.send(`The message with ID ${messageIdToDelete} has been deleted!`);
+  } else {
+    response.send(`Message with ID ${messageIdToDelete} not found!`);
+  }
+});
+app.listen(port, () => {
+  console.log(`listening on PORT ${port}...`);
 });
