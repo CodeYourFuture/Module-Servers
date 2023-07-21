5fd5f09126c30ca4836c3d43847f382dca0b5e52
seyyednavid hejazijouybari <seyyednavidhejazijouybari@gmail.com>
2023-07-21 11:37:42 +0100

    Finishing up to level 5 - advanced validation and define different search fields

--- hotel-bookings-api/server.js
+++ hotel-bookings-api/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { body, validationResult } = require("express-validator");
+const moment = require("moment");
 const cors = require("cors");
 
 const app = express();
@@ -26,9 +27,87 @@ app.get("/bookings", function (request, response) {
   });
 });
 
+app.get("/bookings/search", (req, res) => {
+  if (req.query.date) {
+    const searchDate = req.query.date;
+    if (!moment(searchDate, "YYYY-MM-DD").isValid()) {
+      return res.status(400).json({
+        data: null,
+        message: "Invalid date format. Use YYYY-MM-DD.",
+      });
+    }
+    const bookingsForSearchDate = bookings.filter((booking) =>
+      moment(searchDate).isBetween(booking.checkInDate, booking.checkOutDate)
+    );
+    if (bookingsForSearchDate.length === 0) {
+      return res.status(404).json({
+        data: null,
+        message: "There are no bookings for the given date.",
+      });
+    }
+    res.status(200).json({
+      data: bookingsForSearchDate,
+      message: "Available bookings for the given date",
+    });
+  } else if (req.query.email) {
+    const searchEmail = req.query.email.toLowerCase();
+    const bookingsForSearchEmail = bookings.filter(
+      (booking) => booking.email.toLocaleLowerCase() === searchEmail
+    );
+    if (bookingsForSearchEmail.length === 0) {
+      return res.status(404).json({
+        data: null,
+        message: "There are no bookings for the given email.",
+      });
+    }
+    res.status(200).json({
+      data: bookingsForSearchEmail,
+      message: "Available bookings for the given email",
+    });
+  } else if (req.query.firstName) {
+    const searchFirstName = req.query.firstName.toLowerCase();
+    const bookingsForSearchFirstName = bookings.filter(
+      (booking) => booking.firstName.toLocaleLowerCase() === searchFirstName
+    );
+    if (bookingsForSearchFirstName.length === 0) {
+      return res.status(404).json({
+        data: null,
+        message: "There are no bookings for the given firstName.",
+      });
+    }
+    res.status(200).json({
+      data: bookingsForSearchFirstName,
+      message: "Available bookings for the given firstName",
+    });
+  } else if (req.query.surname) {
+    const searchSurname = req.query.surname.toLowerCase();
+    const bookingsForSearchSurname = bookings.filter(
+      (booking) => booking.surname.toLocaleLowerCase() === searchSurname
+    );
+    if (bookingsForSearchSurname.length === 0) {
+      return res.status(404).json({
+        data: null,
+        message: "There are no bookings for the given surname.",
+      });
+    }
+    res.status(200).json({
+      data: bookingsForSearchSurname,
+      message: "Available bookings for the given surname",
+    });
+  } else {
+    return res.status(400).json({
+      data: null,
+      message:
+        "Yoe can only search based on date, email, firstName and surname and also please input your search content correctly.",
+    });
+  }
+});
+
 app.get("/bookings/:id", (req, res) => {
   const specificBookId = Number(req.params.id);
-  const specificBook = bookings.find((book) => book.id === specificBookId);
+  const specificBook = bookings.find(
+    (booking) => booking.id === specificBookId
+  );
   if (!specificBook) {
     return res.status(404).json({
       data: null,
@@ -88,6 +167,14 @@ app.post(
         message: "Validation error",
       });
     }
+    const checkInDate = moment(req.body.checkInDate);
+    const checkOutDate = moment(req.body.checkOutDate);
+    if (checkOutDate.isBefore(checkInDate)) {
+      return res.status(400).json({
+        data: null,
+        message: "CheckOutDate must be after checkInDate",
+      });
+    }
     bookings.push({
       id: bookings.length + 1,
       ...req.body,
@@ -102,7 +189,7 @@ app.post(
 
 app.delete("/bookings/:id", (req, res) => {
   const deleteBookId = Number(req.params.id);
-  const deleteBook = bookings.find((book) => book.id === deleteBookId);
+  const deleteBook = bookings.find((booking) => booking.id === deleteBookId);
   if (!deleteBook) {
     return res.status(404).json({
       data: null,
