742908d5cc473f7dd651de135deec4ea640e74e8
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-07-23 20:05:03 +0100

    update for level 1
    done find by id
    and create new messages
    and read all the messages

--- chat-server/server.js
+++ chat-server/server.js
@@ -1,10 +1,13 @@
 process.env.PORT = process.env.PORT || 9090;
 const express = require("express");
-const cors = require("cors");
+// const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cors());
 
 const welcomeMessage = {
   id: 0,
@@ -15,12 +18,42 @@ const welcomeMessage = {
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
+
 const messages = [welcomeMessage];
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT,() => {
+app.get("/messages/show", (req, res) => {
+  res.send(messages);
+});
+
+app.get("/messages/:id", (req, res) => {
+  const getById = Number(req.params.id);
+  const findByID = messages.find((message) => message.id === getById);
+  if (!findByID)
+    return res.status(404).send("messages with the ID given was not found");
+  res.send(findByID);
+});
+
+app.post("/messages", (req, res) => {
+  if (!req.body.from || req.body.from.length < 3 || req.body.text.length > 500);
+  res.status(400)
+    .send(
+      "Name is required & should be at least 3 characters & Text should be less then 500 characters"
+    );
+
+  const newMessages = {
+    id: messages.length + 1,
+    from: req.body.from,
+    text: req.body.text,
+  };
+
+  messages.push(newMessages);
+  res.json(messages);
+});
+
+app.listen(process.env.PORT, () => {
   console.log(`listening on PORT ${process.env.PORT}...`);
 });
