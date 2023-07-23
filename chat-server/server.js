1dd66130c546f833f42ad5f5b18f0349746b877f
Khlil1313 <90422263+Khlil1313@users.noreply.github.com>
2023-07-23 21:55:01 +0100

    finish the step-creating a new message

--- chat-server/server.js
+++ chat-server/server.js
@@ -1,13 +1,16 @@
 process.env.PORT = process.env.PORT || 9090;
 const express = require("express");
 const cors = require("cors");
+const bodyparser = require("body-parser");
+const { response, request } = require("express");
 
 const app = express();
 
-app.use(cors());
+app.use(bodyparser.urlencoded({ extended: true }));
+app.use(bodyparser.json());
 
 const welcomeMessage = {
-  id: 0,
+  id: 1,
   from: "Bart",
   text: "Welcome to CYF chat system!",
 };
@@ -15,11 +18,27 @@ const welcomeMessage = {
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
+
 const messages = [welcomeMessage];
+
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
+
+app.get("/messages", (request, response) => {
+  response.json(messages)
+})
+
+app.post("/messages", (request, response) => {
+  const newMessage = {
+    id: messages.length + 1,
+    from: request.body.from,
+    text: request.body.text
+  }
+  messages.push(newMessage)
+  response.send(messages)
+})
 
 app.listen(process.env.PORT,() => {
   console.log(`listening on PORT ${process.env.PORT}...`);
