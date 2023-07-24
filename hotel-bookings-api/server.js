6c77e03952bdb4a67a2cf8d7d97c79f83e79f16c
Delnia Alipour <delniaalipur@gmail.com>
2023-07-24 22:58:01 +0100

    lever 1 completed
    Create a new booking
    Read all bookings
    Read one booking, specified by an ID
    Delete a booking, specified by an ID

--- hotel-bookings-api/server.js
+++ hotel-bookings-api/server.js
@@ -14,7 +14,41 @@ app.get("/", function (request, response) {
 });
 
 // TODO add your routes and helper functions here
-
+//Create a new booking
+app.post("/bookings", function (request, response) {
+  const newBooking = request.body;
+  bookings.push(newBooking);
+  response.send(newBooking);
+});
+//Read all bookings
+app.get("/bookings", function (request, response) {
+  response.send({ bookings });
+});
+//Read one booking by  id
+app.get("/bookings/:id", function (request, response) {
+  const bookingIdToFind = request.params.id;
+  const bookingToFind = bookings.find(
+    (booking) => booking.id === Number(bookingIdToFind)
+  );
+  if (bookingToFind) {
+    response.send(bookingToFind);
+  } else {
+    response.status(404).send();
+  }
+});
+//Delete a booking by Id
+app.delete("/bookings/:id", function (request, response) {
+  const bookingIdToDelete = request.params.id;
+  const bookingToDeleteIndex = bookings.findIndex(
+    (booking) => booking.id === Number(bookingIdToDelete)
+  );
+  if (bookingToDeleteIndex != -1) {
+    bookings.splice(bookingToDeleteIndex, 1);
+    response.status(200).send("Booking has been deleted");
+  } else {
+    response.status(404).send();
+  }
+});
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
