d9a30ff4679c5e1d5a0cda7746dcb1ed16f3a2a9
RahmaB1 <122676773+RahmaB1@users.noreply.github.com>
2023-07-25 02:51:11 +0100

    working on level 3

--- chat-server/server.js
+++ chat-server/server.js
@@ -61,6 +61,22 @@ app.delete("/messages/:id", function (request, response) {
   response.send(messages);
 });
 
+//[error ] Read _only_ messages whose text contains a given substring: `/messages/search?text=express`
+//[not yet ] Read only the most recent 10 messages: `/messages/latest`
+
+app.get("/messages/search", function (request, response) {
+  let searchTerm = request.query.text;
+  console.log("searchTerm:", searchTerm);
+
+  let filteredMsgs = messages.filter(
+    (msg) =>
+      msg.from.toLowerCase().includes(searchTerm.toLowerCase()) ||
+      msg.text.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  response.json(filteredMsgs);
+});
+
 app.listen(process.env.PORT, () => {
   console.log(`listening on PORT ${process.env.PORT}...`);
 });
