48ea0993ccd1e8affbff1b118a990e6a76cb5cd9
AndriusIsin <isin.andrius@gmail.com>
2023-07-25 18:21:27 +0100

    done post and show messages

--- chat-server/server.js
+++ chat-server/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
+app.use(express.json());
 app.use(cors());
 
 const welcomeMessage = {
@@ -16,11 +16,32 @@ const welcomeMessage = {
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
-
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
-
-app.listen(process.env.PORT,() => {
+app.get("/messages", function (request, response) {
+  response.send({ messages });
+});
+// app.get("/messages/:messageId", (req, res) => {
+//   res.send(messages.filter((item) => item["id"] == req.params.messageId));
+// });
+app.get("/messages/:id", function (request, response) {
+  let messageId = Number(request.params.id);
+  const filteredMessages = messages.filter(
+    (message) => message.id === messageId
+  );
+  response.send(filteredMessages);
+});
+app.post("/messages", function (request, response) {
+  let newMessage = {
+    id: messages.length,
+    from: request.body.from,
+    text: request.body.text,
+  };
+  messages.push(newMessage);
+  response.json(messages);
+  console.log(messages);
+});
+app.listen(process.env.PORT, () => {
   console.log(`listening on PORT ${process.env.PORT}...`);
 });
