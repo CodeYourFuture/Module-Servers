f20da22d76d358f08d44d89897dce4d4ae28aec9
RahmaB1 <122676773+RahmaB1@users.noreply.github.com>
2023-07-25 01:11:23 +0100

    done chat server

--- chat-server/server.js
+++ chat-server/server.js
@@ -1,10 +1,14 @@
 process.env.PORT = process.env.PORT || 9090;
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
-
+// app.use(bodyParser.json());
 app.use(cors());
+
+// [ ] All message content should be passed as JSON.
+app.use(express.json());
 
 const welcomeMessage = {
   id: 0,
@@ -12,15 +16,42 @@ const welcomeMessage = {
   text: "Welcome to CYF chat system!",
 };
 
-//This array is our "data store".
-//We will start with one message in the array.
-//Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+//- [ ] Read all messages
+app.get("/messages", function (request, response) {
+  response.send(messages);
 });
 
-app.listen(process.env.PORT,() => {
+//[ ] Create a new message
+app.post("/messages", function (request, response) {
+  const messegeFromUser = {
+    id: messages.length,
+    from: request.body.from,
+    text: request.body.text,
+  };
+  messages.push(messegeFromUser);
+  response.send(messages);
+});
+
+//- [ ] Read one message specified by an ID
+app.get("/messages/:id", function (request, response) {
+  const id = Number(request.params.id);
+  const searched = messages.filter((msg) => msg.id === id);
+  console.log(searched);
+  response.send(searched);
+});
+
+//  [ ] Delete a message, by ID
+app.delete("/messages/:id", function (request, response) {
+  const id = Number(request.params.id);
+  const indexToDelete = messages.findIndex((msg) => msg.id === id);
+  if (indexToDelete !== -1) {
+    messages.splice(indexToDelete, 1);
+  }
+  response.send(messages);
+});
+
+app.listen(process.env.PORT, () => {
   console.log(`listening on PORT ${process.env.PORT}...`);
 });
