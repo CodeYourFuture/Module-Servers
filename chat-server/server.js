331876a6b7dd9a778ecb4d8d52417c5426ee0693
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-07-25 20:15:31 +0100

    update

--- chat-server/server.js
+++ chat-server/server.js
@@ -24,24 +24,29 @@ const messages = [welcomeMessage];
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
+
 // show all
 app.get("/messages", function (req, res) {
   res.send(messages);
 });
+
 // find by id
 app.get("/messages/:id", (req, res) => {
   const getById = Number(req.params.id);
   const findByID = messages.find((message) => message.id === getById);
   if (!findByID)
-    return res.status(404).send("messages with the ID given was not found");
-  res.send(findByID);
+    return res.status(404).json("messages with the ID given was not found");
+  res.json(findByID);
 });
+
 // create new
 app.post("/messages/cerate", (req, res) => {
-  if (!req.body.from || req.body.from.length < 3)
+  if (!req.body.from || req.body.from.length < 3 || req.body.text === "")
     return res
       .status(400)
-      .send("Name is required & should be at least 3 characters");
+      .json(
+        "Name is required & should be at least 3 characters & should have to write message"
+      );
 
   const newMessages = {
     id: messages.length + 1,
@@ -52,20 +57,35 @@ app.post("/messages/cerate", (req, res) => {
   messages.push(newMessages);
   res.json(messages);
 });
+
 // delete by id
 app.delete("/messages/delete/:id", (req, res) => {
   const getById = Number(req.params.id);
-  console.log(getById);
   const findByID = messages.find((message) => message.id === getById);
   const index = messages.indexOf(findByID);
 
   if (!findByID)
-    return res.status(404).send("messages with the ID given was not found");
+    return res.status(404).json("message with the ID given was not found");
 
   messages.splice(index, 1);
-  res.status(200).send("messages with the ID has been deleted");
+  res.status(200).json("message with the ID has been deleted");
 });
 
+//search
+app.get("/messages/search", (req, res) => {
+  const message = req.query.text;
+  
+    if (message.length < 0) {
+      return res
+        .status(400)
+        .json("Please provide a 'text' parameter for search.");
+    }
+
+    const searchResult = messages.filter((text) =>
+      text.text.toLowerCase().includes(message.toLowerCase())
+    );
+  res.json(searchResult);
+});
 
 app.listen(process.env.PORT, () => {
   console.log(`listening on PORT ${process.env.PORT}...`);
