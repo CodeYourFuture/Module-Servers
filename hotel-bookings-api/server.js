f63cccd3442d36cac94562a5e3b08a3e49f0d6de
Shadi38 <113238379+Shadi38@users.noreply.github.com>
2023-07-26 11:41:16 +0100

    read all booking,creat new booking,read book with specified by an id

--- hotel-bookings-api/server.js
+++ hotel-bookings-api/server.js
@@ -12,8 +12,32 @@ const bookings = require("./bookings.json");
 app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
+//read all booking
+app.get("/bookings",function (req,res){
+  if(bookings.length===0){
+    return res.status(404).json({
+      error:"there is no booking"
+    })
+  }
+res.json({bookings}); 
+})
 
-// TODO add your routes and helper functions here
+//create new message
+app.post("/bookings",function (req,res) {
+  bookings.push(req.body);
+  res.json({bookings});
+})
+
+//Read one booking, specified by an ID
+app.get("/bookings/:id",function (req,res) {
+  const choosedBooking = bookings.find(booking=>booking.id == req.params.id);
+  if(!choosedBooking){
+    return res.status(404).json({
+      error:"booking with this id wasn't found"
+    })
+  }
+  res.json({choosedBooking});
+})
 
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
