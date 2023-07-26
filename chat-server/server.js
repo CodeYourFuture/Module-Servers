979b1d6f3ef84da0e91ee3fa1665bc9f46fc7583
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-07-26 15:21:41 +0100

    added react

--- chat-server/server.js
+++ chat-server/server.js
@@ -1,10 +1,18 @@
 process.env.PORT = process.env.PORT || 9090;
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+
 
 const app = express();
 
 app.use(cors());
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
 
 const welcomeMessage = {
   id: 0,
@@ -20,6 +28,34 @@ const messages = [welcomeMessage];
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
+
+app.post("/messages", function (request, response) {
+  let newMessage = request.body;
+  messages.push(newMessage);
+  response.json(newMessage);
+  console.log(newMessage)
+});
+
+//read all messages
+app.get("/messages", function (request, response) {
+  response.json(messages);
+});
+
+app.get("/messages/:id", function (request, response) {
+  const idToFind = Number(request.params.id)
+  const message = messages.find((message) => message.id === idToFind)
+  console.log(message)
+  response.json(message)
+})
+
+app.delete("/messages/:id", function (request, response) {
+  const idToFind = Number(request.params.id);
+  const message = messages.find((message) => message.id === idToFind);
+  console.log(message);
+  response.json(message);
+});
+
+
 
 app.listen(process.env.PORT,() => {
   console.log(`listening on PORT ${process.env.PORT}...`);
