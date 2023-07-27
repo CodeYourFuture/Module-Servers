7962fd7ea0a58a3401fedaf1470b3caa16fcd640
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-07-27 15:11:02 +0100

    hotel udate
    get all booking and show as json

--- hotel-bookings-api/server.js
+++ hotel-bookings-api/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const fs = require("fs");
+const bookings = JSON.parse(fs.readFileSync("./bookings.json"))
 const cors = require("cors");
 
 const app = express();
@@ -7,14 +9,19 @@ app.use(express.json());
 app.use(cors());
 
 //Use this array as your (in-memory) data store.
-const bookings = require("./bookings.json");
+// const bookings = require("./bookings.json");
 
 app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 
+// show all bookings
+app.get('/api/hotel', (req, res) => {
+  res.send(bookings)
+})
+
 // TODO add your routes and helper functions here
 
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+
+const port = process.env.PORT || 5099;
+app.listen(port, () => console.log(`listen on port ${port} .....!!`));
