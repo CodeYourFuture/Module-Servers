5e7cac9f9662a40416d9255bddd47dc437458a9b
Olha Danylevska <odanilevskaya2@gmail.com>
2023-07-28 22:30:06 +0100

    level1 for hotel-bookings

--- hotel-bookings-api/server.js
+++ hotel-bookings-api/server.js
@@ -13,8 +13,45 @@ app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 
+app.get("/bookings", (req, res) => {
+  res.send(bookings)
+})
+
+app.post("/bookings", (req, res) => {
+  let newBooking = req.body
+  bookings && bookings.push(newBooking)
+  res.send(newBooking)
+})
+
+app.get("/bookings/:id", (req, res) => {
+  let bookingID = req.params.id
+  let singleBooking = bookings.filter(booking => {
+    return booking.id === Number(bookingID)
+  })
+  if (singleBooking.length === 0) {
+    res.status(404).send("Not found. 404");
+  } else {
+    res.send(singleBooking)
+  }
+})
+
+app.delete("/bookings/:id", (req, res) => {
+  let givenId = req.params.id
+  res.send(deleteBookingID(bookings, givenId))
+})
+
 // TODO add your routes and helper functions here
 
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
+
+
+const deleteBookingID = (bookings, id) => {
+  let bookingInd = bookings.findIndex((booking) => booking.id == id);
+
+  if (bookingInd > -1) {
+    bookings.splice(bookingInd, 1);
+  }
+  return bookings;
+}
