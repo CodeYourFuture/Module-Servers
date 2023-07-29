bb2ccf69b260655eca3f4c02713606fba74d71e4
Khlil1313 <90422263+Khlil1313@users.noreply.github.com>
2023-07-29 12:44:14 +0100

    finshed the validation of level2

--- chat-server/server.js
+++ chat-server/server.js
@@ -18,45 +18,47 @@ const welcomeMessage = {
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-const messages = []
+const messages = [];
 messages.push(welcomeMessage);
-
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
 app.get("/messages", (request, response) => {
-  response.json(messages)
-})
+  response.json(messages);
+});
 
 app.get("/messages/:id", (request, response) => {
   const search = Number(request.params.id);
-  const messageWithSpecificId = messages.find((e) => e.id === search)
-  response.json(messageWithSpecificId)
-})
+  const messageWithSpecificId = messages.find((e) => e.id === search);
+  response.json(messageWithSpecificId);
+});
 
 app.delete("/messages/:id", (request, response) => {
   const newMessageId = messages.findIndex(
     (e) => e.id === Number(request.params.id)
   );
   if (newMessageId >= 0) {
-   response.json(messages.splice(newMessageId, 1))
+    response.json(messages.splice(newMessageId, 1));
   }
-})
+});
 
 app.post("/messages", (request, response) => {
   const newMessage = {
     id: messages.length + 1,
     from: request.body.from,
-    text: request.body.text
+    text: request.body.text,
+  };
+  if (newMessage.from.trim() === "" || newMessage.text.trim() === "") {
+    response.sendStatus(400)
+  } else {
+    messages.push(newMessage);
+    response.send(messages);
   }
-  messages.push(newMessage)
-  response.send(messages)
-})
+  
+});
 
-
-
-app.listen(process.env.PORT,() => {
+app.listen(process.env.PORT, () => {
   console.log(`listening on PORT ${process.env.PORT}...`);
 });
