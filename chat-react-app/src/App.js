b870bc96bd37a2112fe1c0ab9bb391b9276fec1e
AppolinFotso <appolin.s.fotso@gmail.com>
2023-07-29 14:03:44 +0100

    Added Edit and Delete buttons

--- chat-react-app/src/App.js
+++ chat-react-app/src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import Chat from "./Chats";
 import Search from "./SearchChat";
@@ -7,12 +6,18 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [posted, setPosted] = useState([]);
-  // post latest
+  const [id, setId] = useState(1.1);
+
+  const [from, setFrom] = useState("");
+  const [text, setText] = useState("");
+
+  // update
 
   // post a message
-  function handleSubmit(f, t) {
+  function handleSubmit(i, f, t) {
     console.log(f, t);
     let data = new URLSearchParams();
+    data.append("id", i);
     data.append("from", f);
     data.append("text", t);
     // let formData = new FormData();
@@ -43,11 +48,25 @@ function App() {
           <Search latest={setPosted} />
         </div>
         <div className="chat-messages">
-          <Chat postMe={posted} />
+          <Chat
+            delete={setPosted}
+            postMe={posted}
+            updateId={setId}
+            updateFrom={setFrom}
+            updateText={setText}
+          />
         </div>
 
         <div className="chat-form">
-          <Form Submit={handleSubmit} />
+          <Form
+            Submit={handleSubmit}
+            id={id}
+            from={from}
+            text={text}
+            updateId={setId}
+            updateFrom={setFrom}
+            updateText={setText}
+          />
         </div>
       </div>
     </div>
--- chat-react-app/src/Form.js
+++ chat-react-app/src/Form.js
@@ -9,24 +9,48 @@ function Form(prop) {
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          prop.Submit(e.target.from.value, e.target.text.value);
+          prop.Submit(
+            e.target.id.value,
+            e.target.from.value,
+            e.target.text.value
+          );
           e.target.from.value = "";
           e.target.text.value = "";
+          prop.updateId(1.1);
+          prop.updateFrom("");
+          prop.updateText("");
         }}
       >
         <p>
           <input
+            className="shape-btn name id"
+            type="number"
+            name="id"
+            value={prop.id}
+            placeholder="Your ID"
+          />
+        </p>
+        <p>
+          <input
+            onChange={(e) => {
+              prop.updateFrom(e.target.value);
+            }}
             className="shape-btn name"
             type="text"
             name="from"
+            value={prop.from}
             placeholder="Your Name"
           />
         </p>
         <p>
           <textarea
+            onChange={(e) => {
+              prop.updateText(e.target.value);
+            }}
             className="shape-btn message"
             type="text"
             name="text"
+            value={prop.text}
             placeholder="The message..."
           ></textarea>
           <button type="submit" className="shape-btn">
