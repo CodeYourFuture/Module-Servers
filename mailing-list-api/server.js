729c1030f5afa209e62505341321703f9028a5c3
Delnia Alipour <delniaalipur@gmail.com>
2023-07-29 20:29:00 +0100

    Get single list

--- mailing-list-api/server.js
+++ mailing-list-api/server.js
@@ -15,6 +15,23 @@ app.get("/lists", function (request, response) {
   }
 });
 
+//GET single list
+app.get("/lists/:name", function (request, response) {
+  const singleListName = request.params.name;
+  const nameIndex = Object.keys(mailListObject).indexOf(singleListName);
+  console.log(nameIndex);
+  if (nameIndex !== -1) {
+    const singleListValue = Object.values(mailListObject)[nameIndex];
+    const singleObject = {
+      name: singleListName,
+      members: singleListValue,
+    };
+    response.status(200).send(singleObject);
+  } else {
+    response.status(404).json({ error: "List not found" });
+  }
+});
+
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
