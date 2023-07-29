4d80acce6f51c3436fc718c45a7a79de140f6d49
Hussein Bahdon <husseinbahdon1@gmail.com>
2023-07-29 11:54:11 +0100

    Update server.js

--- quote-server/server.js
+++ quote-server/server.js
@@ -1,34 +1,41 @@
-// server.js
-// This is where your node app starts
 
-//load the 'express' module which makes writing webservers easy
 const express = require("express");
-const app = express();
-
-//load the quotes JSON
 const quotes = require("./quotes.json");
+const PORT = 3030;
 
-// Now register handlers for some routes:
-//   /                  - Return some helpful welcome info (text)
-//   /quotes            - Should return all quotes (json)
-//   /quotes/random     - Should return ONE quote (json)
+const app = express();
+const lodash = require('lodash');
+
+
 app.get("/", function (request, response) {
   response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
 });
 
-//START OF YOUR CODE...
+app.get("/quotes", function (req, res){
+    res.send(quotes)
+})
 
-//...END OF YOUR CODE
+app.get("/quotes/random", function (req, res) {
+  res.send(lodash.sample(quotes))
+})
 
-//You can use this function to pick one element at random from a given array
-//example: pickFromArray([1,2,3,4]), or
-//example: pickFromArray(myContactsArray)
-//
+app.get("/quotes/search", function(req, res) {
+  const term = req.query.term
+  // console.log(term)
+  const resultSearchQuotes = searchQuotes(term)
+  res.send(resultSearchQuotes)
+})
+
+function searchQuotes(term){
+  const results = quotes.filter(element => element.quote.toLowerCase().includes(term.toLowerCase()) || element.author.toLowerCase().includes(term.toLowerCase()))
+  return results;
+}
+
 function pickFromArray(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-//Start our server so that it listens for HTTP requests!
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+
+const listener = app.listen(PORT, function () {
+  console.log("Your app is listening on port " + PORT);
 });
