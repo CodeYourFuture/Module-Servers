4a6c14313b55d1c4d63d0aeef911c24378ed6546
Olha Danylevska <odanilevskaya2@gmail.com>
2023-07-31 20:15:23 +0100

    e-mail validator

--- hotel-bookings-api/server.js
+++ hotel-bookings-api/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const moment = require('moment');
 
 const app = express();
 
@@ -19,11 +20,43 @@ app.get("/bookings", (req, res) => {
 
 app.post("/bookings", (req, res) => {
   let newBooking = req.body
-  if (newBooking.title === "" || newBooking.firstName === "" || newBooking.surname === "" || newBooking.email === "" || newBooking.roomId === "" || newBooking.checkInDate === "" || newBooking.checkOutDate === "") {
+
+  let a = moment(newBooking.checkOutDate)
+  let b = moment(newBooking.checkInDate)
+
+  const emailToValidate = newBooking.email;
+  const emailRegexp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+  if (!moment(a).isAfter(b)) {
+    res.status(400).send("Check out date should be after Check in date");
+  } else if (emailRegexp.test(emailToValidate) == false) {
+    res.status(400).send("Please provide a valid email address.")
+  } else if (newBooking.title === "" || newBooking.firstName === "" || newBooking.surname === "" || newBooking.email === "" || newBooking.roomId === "" || newBooking.checkInDate === "" || newBooking.checkOutDate === "" ||
+    !newBooking.title || !newBooking.firstName || !newBooking.surname || !newBooking.email || !newBooking.roomId || !newBooking.checkInDate || !newBooking.checkOutDate
+  ) {
+    res.status(400).send("Didn't store the booking in the bookings array");
+  } else {
     bookings && bookings.push(newBooking)
     res.send(newBooking)
-  } else {
-    res.status(400).send("Didn't store the booking in the bookings array");
+  }
+})
+
+app.get("/bookings/search", (req, res) => {
+  let searchDate = req.query.date
+  let searchTheName = req.query.term
+
+  if (searchDate) {
+    let singleDate = bookings.filter(booking => {
+      return moment(searchDate).isBetween(booking.checkInDate, booking.checkOutDate);
+    })
+    res.send(singleDate)
+  } else if (searchTheName) {
+    let singleName = bookings.filter(booking => {
+      return booking.email.toLowerCase().includes(searchTheName.toLowerCase())
+        || booking.firstName.toLowerCase().includes(searchTheName.toLowerCase())
+        || booking.surname.toLowerCase().includes(searchTheName.toLowerCase())
+    })
+    res.send(singleName)
   }
 
 })
