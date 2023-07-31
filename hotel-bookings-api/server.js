9e20b8336fdc62b2efd376b9c6c7fb12bc764916
Abubakar-Meigag <dr.sco0ot.77@gmail.com>
2023-07-31 16:02:57 +0100

    add validation for post and tested

--- hotel-bookings-api/server.js
+++ hotel-bookings-api/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const {body, validationResult} = require('express-validator');
 const fs = require("fs");
 const bookings = JSON.parse(fs.readFileSync("./bookings.json"));
 const cors = require("cors");
@@ -22,9 +23,49 @@ app.get("/api/hotel/booking", (req, res) => {
 
 // TODO add your routes and helper functions here
 
+const validationData = [
+  body("title").trim().notEmpty(),
+  body("firstName")
+    .trim()
+    .notEmpty()
+    .isLength({ min: 3 })
+    .withMessage("First Name must be at less 3 characters"),
+  body("surname")
+    .trim()
+    .notEmpty()
+    .isLength({ min: 3 })
+    .withMessage("Surname must be at less 3 characters"),
+  body("email")
+    .trim()
+    .notEmpty()
+    .isEmail()
+    .toLowerCase()
+    .normalizeEmail()
+    .withMessage("Email must be a valid email"),
+  body("roomId").trim().notEmpty().isNumeric(),
+  body("checkInDate").trim().notEmpty(),
+  body("checkOutDate").trim().notEmpty(),
+];
+
 //post
-app.post("/api/hotel/booking", (req, res) => {
+app.post("/api/hotel/booking", validationData, (req, res) => {
+
+  const error = validationResult(req);
+  if (!error.isEmpty()) {
+    return res.status(400).json({ error: error.array() });
+  };
+
   const newId = bookings[bookings.length - 1].id + 1;
+  const {
+    title,
+    firstName,
+    surname,
+    email,
+    roomId,
+    checkInDate,
+    checkOutDate,
+  } = req.body;
+
   const newBooking = Object.assign({ id: newId }, req.body);
 
   bookings.push(newBooking);
@@ -64,8 +105,8 @@ app.delete("/api/hotel/booking/:id", (req, res) => {
   fs.writeFile("./bookings.json", JSON.stringify(bookings), () => {
     res.status(200).send({
       bookings: {
-        Message: "Booking with id has been deleted successfully"
-      }
+        Message: "Booking with id has been deleted successfully",
+      },
     });
   });
 });
