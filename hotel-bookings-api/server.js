0c2bcfbc52c327d3c268a6dd83a2de7c2b22e2c4
Saliha Popal <salihapopal@gmail.com>
2023-08-02 23:01:22 +0100

    Level-1

--- hotel-bookings-api/server.js
+++ hotel-bookings-api/server.js
@@ -13,6 +13,49 @@ app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
 
+// Route to post new booking
+app.post("/bookings", (request, response) => {
+  const { firstName, surname, title, email, roomId, checkInDate, checkOutDate} = request.body; 
+
+  if (!firstName|| !surname|| !title || !email || !roomId || !checkInDate || !checkOutDate) {
+    return response.status(400).send("Please provide valid 'from' and 'text' fields in the form.");
+  }
+  const newBooking = {
+    id: bookings.length +1, 
+      title,
+      firstName,
+      surname,
+      email,
+      roomId,
+      checkInDate,
+      checkOutDate
+  };
+  bookings.push(newBooking);
+  console.log(bookings); 
+  response.redirect("/"); 
+});
+
+//Route to get all bookings
+app.get("/bookings", function(request, response) {
+  response.json({bookings});
+});
+
+// route to get booking by id
+app.get("/bookings/:id", function(request, response) {
+  const idToFind = Number(request.params.id)
+  const booking = bookings.find((booking) => booking.id === idToFind);
+  console.log(booking);
+  response.send(booking);
+});
+
+// Route to delete bookings by id
+app.delete("/bookings/:id", function(request, response) {
+  const idToDelete = Number(request.params.id);
+  const deletedBooking = bookings.find((booking)=> booking.id === idToDelete);
+  response.status(204).send(deletedBooking);
+})
+
+
 // TODO add your routes and helper functions here
 
 const listener = app.listen(process.env.PORT, function () {
