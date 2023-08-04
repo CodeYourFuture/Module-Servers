2b45e7ed3d844cc3acc8bed1630be7e674281a44
Junitalama <113072416+Junitalama@users.noreply.github.com>
2023-08-04 20:00:37 +0100

    installed json body parser

--- hotel-bookings-api/server.js
+++ hotel-bookings-api/server.js
@@ -1,11 +1,14 @@
 process.env.PORT = process.env.PORT || 9090;
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser")
 
 const app = express();
 
-app.use(express.json());
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
 
 //Use this array as your (in-memory) data store.
 const bookings = require("./bookings.json");
