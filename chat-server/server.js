eff883c60d17abf3be477dec7d95ff5a7057681c
fowobi <109865505+fowobi@users.noreply.github.com>
2023-08-06 00:29:37 +0100

    Update server.js

--- chat-server/server.js
+++ chat-server/server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 let corsOptions = {
-  origin: "awesome-cyf-website.com", // Compliant
+  origin:"awesome-cyf-website.com", // Compliant
   
 };
 
