0c6ffb515ac3eb6b1133dc98efab71ee889bef50
nasir ali <a.nasirali@outlook.com>
2023-08-07 16:11:23 +0100

    made the main changes and added new functionality

--- chat-server/server.js
+++ chat-server/server.js
@@ -1,6 +1,7 @@
-process.env.PORT = process.env.PORT || 9090;
 const express = require("express");
 const cors = require("cors");
+const fs = require("fs");
+const bodyParser = require("body-parser");
 
 const app = express();
 
@@ -8,19 +9,54 @@ app.use(cors());
 
 const welcomeMessage = {
   id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
+  sender: "Bart",
+  text: "Welcome to the chat system!",
 };
 
-//This array is our "data store".
-//We will start with one message in the array.
-//Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT,() => {
-  console.log(`listening on PORT ${process.env.PORT}...`);
+app.get("/messages", (req, res) => {
+  fs.readFile("messages.json", async (err, data) => {
+    try {
+      const messages = await JSON.parse(data);
+      res.json(messages);
+    } catch (err) {
+      console.log(err);
+    }
+  });
+});
+
+app.post("/messages", (req, res) => {
+  if (req.body.sender !== "" && req.body.text !== "") {
+    fs.readFile("messages.json", async (err, data) => {
+      try {
+        const messages = await JSON.parse(data);
+        const newMessage = {
+          id: messages.length,
+          sender: req.body.sender,
+          text: req.body.text,
+          timeSent: new Date(),
+        };
+        messages.push(newMessage);
+        fs.writeFile("messages.json", JSON.stringify(messages), (err) => {
+          if (err) {
+            console.log(err);
+          }
+        });
+        res.json([newMessage]);
+      } catch (err) {
+        console.log(err);
+      }
+    });
+  } else {
+    res.json([{ statusCode: 400, error: "Form field empty" }]);
+  }
+});
+
+app.listen(process.env.PORT || 9090, () => {
+  console.log(`Listening on port ${process.env.PORT || 9090}...`);
 });
