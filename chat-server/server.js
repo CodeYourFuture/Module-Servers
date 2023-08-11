878cbb7892d0363ce9abce5299c250247ede4da9
Stella Del Mar <113214552+Stelladelmar@users.noreply.github.com>
2023-08-11 21:55:36 +0100

    Char-Server ex-1

--- chat-server/server.js
+++ chat-server/server.js
@@ -5,12 +5,14 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.json())
 
 const welcomeMessage = {
   id: 0,
   from: "Bart",
   text: "Welcome to CYF chat system!",
 };
+
 
 //This array is our "data store".
 //We will start with one message in the array.
@@ -20,6 +22,21 @@ const messages = [welcomeMessage];
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
+
+app.get("/messages", function(request, response){
+  response.send(messages);
+})
+app.get("/messages/:id", function(request, response){
+  let id = Number(request.params.id)
+  console.log(request)
+  let message = messages.find(message => message.id=== id)
+  response.send(message);
+
+})
+app.post("/messages", function(request, response){
+  response.send("tHANK YOU");
+  messages.push(request.body);
+})
 
 app.listen(process.env.PORT,() => {
   console.log(`listening on PORT ${process.env.PORT}...`);
