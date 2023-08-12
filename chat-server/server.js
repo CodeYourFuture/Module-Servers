a77c8ced896656a9356cab9b78386fb911cca12d
PaulinaWywrot <wywrot.paula@gmail.com>
2023-08-12 15:28:47 +0100

    level 1

--- chat-server/server.js
+++ chat-server/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 const welcomeMessage = {
   id: 0,
@@ -21,6 +22,30 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT,() => {
+app.get("/messages", function (request, response) {
+  response.status(200).json(messages);
+});
+app.get("/messages/:id", function (request, response) {
+  let id = Number(request.params.id);
+  let message = messages.find((message) => message.id === id);
+  response.status(200).send(message);
+});
+app.post("/messages", function (request, response) {
+  let newMessage = {
+    id: messages.length,
+    from: request.body.from,
+    text: request.body.text,
+  };
+  if (request.body.from && request.body.text) {
+    messages.push(newMessage);
+    response.json(messages);
+  } else response.status(400).send("Please fill in missing details");
+});
+app.delete("/messages/:id", function (request, response) {
+  let id = Number(request.params.id);
+  let message = messages.findIndex((message) => message.id === id);
+  response.json(messages.splice(message, 1));
+});
+app.listen(process.env.PORT, () => {
   console.log(`listening on PORT ${process.env.PORT}...`);
 });
