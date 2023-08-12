a494aa94d99705701bb1d9d12109d33d61afe6a8
PaulinaWywrot <wywrot.paula@gmail.com>
2023-08-12 20:10:02 +0100

    Levels 1 & 2

--- hotel-bookings-api/server.js
+++ hotel-bookings-api/server.js
@@ -1,3 +1,4 @@
+process.env.PORT = process.env.PORT || 9090;
 const express = require("express");
 const cors = require("cors");
 
@@ -12,7 +13,47 @@ const bookings = require("./bookings.json");
 app.get("/", function (request, response) {
   response.send("Hotel booking server.  Ask for /bookings, etc.");
 });
-
+app.get("/bookings", function (request, response) {
+  response.send(bookings);
+});
+app.get("/bookings/:id", function (request, response) {
+  let id = Number(request.params.id);
+  let booking = bookings.find((booking) => booking.id === id);
+  if (booking) {
+    response.json(booking);
+  } else response.status(404).send("Booking does not exist");
+});
+app.post("/bookings", function (request, response) {
+  let newBooking = {
+    id: bookings.length + 1,
+    title: request.body.title,
+    firstName: request.body.firstName,
+    surname: request.body.surname,
+    email: request.body.email,
+    roomId: request.body.roomId,
+    checkInDate: request.body.checkInDate,
+    checkOutDate: request.body.checkOutDate,
+  };
+  if (
+    request.body.title &&
+    request.body.firstName &&
+    request.body.surname &&
+    request.body.email &&
+    request.body.roomId &&
+    request.body.checkInDate &&
+    request.body.checkOutDate
+  ) {
+    bookings.push(newBooking);
+    response.json(newBooking);
+  } else response.status(404).send("Missing data, please check all fields are filled in");
+});
+app.delete("/bookings/:id", function (request, response) {
+  let id = Number(request.params.id);
+  let IndexOfBooking = bookings.findIndex((booking) => booking.id === id);
+  if (IndexOfBooking > 0) {
+    response.json(IndexOfBooking.splice(IndexOfBooking, 1));
+  } else response.status(404).send("Booking does not exist");
+});
 // TODO add your routes and helper functions here
 
 const listener = app.listen(process.env.PORT, function () {
