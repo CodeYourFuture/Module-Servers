b4e4c41609c99138f0c58644aa843a1844903683
Bedi Omuri <113227370+Bedi06@users.noreply.github.com>
2023-08-14 00:56:30 +0100

    level 1

--- hotel-bookings-api/server.js
+++ hotel-bookings-api/server.js
@@ -15,6 +15,42 @@ app.get("/", function (request, response) {
 
 // TODO add your routes and helper functions here
 
+app.post("/bookings",function (request,response){
+  const newBooking=request.body;
+  bookings.push(newBooking);
+  response.status(201).send({newBooking});
+});
+
+app.get("/bookings",function(request,response){
+  response.send(bookings);
+});
+
+app.get("/bookings",function(request,response){
+  const bookingId=parseInt(request.params.id);
+  const booking=bookings.find((b)=>bookingId);
+
+
+  if (!booking) {
+    return response.status(404).send({ error: "Booking not found" });
+  }
+
+  response.send(booking);
+
+})
+
+app.delete("/bookings/:id",function(request,response){
+  const bookingId=parseInt(request.params.id);
+  const bookingIndx=bookings.findIndex((b) => b.id === bookingId);
+
+  if (bookingIndx === -1) {
+    return res.status(404).send({ error: "Booking not found" });
+  }
+
+  const deletedBooking = bookings.splice(bookingIndx, 1)[0];
+  res.send(deletedBooking);
+});
+
+
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
