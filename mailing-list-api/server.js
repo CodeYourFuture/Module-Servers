05ef3e26dc7d32304a25d5937936910ebfc0f0c0
AppolinFotso <appolin.s.fotso@gmail.com>
2023-08-18 09:59:07 +0100

    Added the /lists route

--- mailing-list-api/server.js
+++ mailing-list-api/server.js
@@ -1,16 +1,21 @@
 // server.js
 const cors = require("cors");
-const lodash = require("lodash");
 const express = require("express");
 const app = express();
 
 //loading the mailing list JSON
-const mailingList = require("./mailing-list");
+const mailingList = require("./mailing-lists");
 // middleware
 app.use(cors());
 
 // registering handlers for some routes:
-
+app.get("/lists", (req, res) => {
+  const listOfNames = [];
+  for (let name of Object.keys(mailingList)) {
+    listOfNames.push(listOfNames);
+  }
+  res.json(empty);
+});
 //Start our server so that it listens for HTTP requests!
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
