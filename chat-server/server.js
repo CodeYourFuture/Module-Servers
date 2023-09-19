786842517930439c3573ef9103753947eaf6dc1c
onurat <114289826+onurat@users.noreply.github.com>
2023-09-19 17:47:41 +0100

    .

--- chat-server/server.js
+++ chat-server/server.js
@@ -5,22 +5,58 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 const welcomeMessage = {
   id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
+  from: "Onur Atas",
+  text: "Welcome to my chat system!",
 };
 
-//This array is our "data store".
-//We will start with one message in the array.
-//Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT,() => {
+app.post("/messages", (req, res) => {
+  const { from, text } = req.body;
+  if (!from || !text) {
+    return res.status(400).json({ error: 'Both "from" and "text" fields are required.' });
+  }
+
+  const newMessage = {
+    id: messages.length,
+    from,
+    text,
+  };
+
+  messages.push(newMessage);
+  res.status(201).json(newMessage);
+});
+
+app.get("/messages", (req, res) => {
+  res.json(messages);
+});
+
+app.get("/messages/:id", (req, res) => {
+  const messageId = parseInt(req.params.id);
+  const message = messages.find((msg) => msg.id === messageId);
+  if (!message) {
+    return res.status(404).json({ error: 'Message not found.' });
+  }
+  res.json(message);
+});
+
+app.delete("/messages/:id", (req, res) => {
+  const messageId = parseInt(req.params.id);
+  const index = messages.findIndex((msg) => msg.id === messageId);
+  if (index === -1) {
+    return res.status(404).json({ error: 'Message not found.' });
+  }
+  messages.splice(index, 1);
+  res.status(204).send();
+});
+app.listen(process.env.PORT, () => {
   console.log(`listening on PORT ${process.env.PORT}...`);
 });
