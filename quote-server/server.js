485ae40c87d3cbd9fdafeae9e58c3e5d1cfc0a4d
= <adniyayousaf92@gmail.com>
2024-03-19 17:31:15 +0000

    Install cors in repo

--- quote-server/server.js
+++ quote-server/server.js
@@ -1,48 +1,33 @@
-// server.js
-// This is where your node app starts
-
 import express, { query } from "express";
-//load the quotes JSON
+import cors from "cors"
 import quotes from "./quotes.json" assert { type: "json" };
 
-const app = express();
 
+
+const app = express();
+app.use(cors());
 app.get("/quotes/search", (request, response) => {
   const n = request.query.term;
-  response.send(searchQuote(n));
+  response.send(searchQuote(n.toLocaleLowerCase()));
 });
 
 app.get("/quotes/random", (request, response) => {
   response.send(pickFromArray(quotes));
 });
 
-
 app.get("/quotes", (request, response) => {
   response.send(quotes);
 });
 
 app.get("/", (request, response) => {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes or /quotes/search");
+  response.send("Adniya's Quote Server!  Ask me for /quotes/random, or /quotes or /quotes/search");
 });
 
-app.get("/hello", (request, response) => {
-  const n = request.query.name;
-  response.send("Hello!  I'm a sever that just says hello to " + n);
-});
-
-
-
-//...END OF YOUR CODE
-
-//You can use this function to pick one element at random from a given array
-//example: pickFromArray([1,2,3,4]), or
-//example: pickFromArray(myContactsArray)
-//
 const pickFromArray = (arrayofQuotes) =>
   arrayofQuotes[Math.floor(Math.random() * arrayofQuotes.length)];
 
 const searchQuote = (searchTerm) =>
-  quotes.filter((quote) => quote.quote.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()) || quote.author.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()));
+  quotes.filter((quote) => quote.quote.toLocaleLowerCase().includes(searchTerm) || quote.author.toLocaleLowerCase().includes(searchTerm));
 
 
 //Start our server so that it listens for HTTP requests!
