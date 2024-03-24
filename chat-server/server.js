190592be437f53f52e79c485b72be410fd63b932
Marcus <marcus.ats94@gmail.com>
2024-03-24 20:52:38 +0000

    Completed level 1 for the chat-server-project

--- chat-server/server.js
+++ chat-server/server.js
@@ -1,28 +1,47 @@
 process.env.PORT = process.env.PORT || 9090;
 import express from "express";
 import cors from "cors";
-import path from "path";
 import { fileURLToPath } from "url";
+import path from "path";
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 // Get __dirname in ES module
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const welcomeMessage = {
-  id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
-};
-
-//This array is our "data store".
-//We will start with one message in the array.
-const messages = [welcomeMessage];
+let messages = [];
+let count = -1;
 
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/index.html");
+});
+
+app.get("/messages", (req, res) => {
+  res.send(messages);
+});
+
+app.get("/messages/:messageId", (req, res) => {
+  const messageId = Number(req.params.messageId);
+  const filterMessages = messages.find((message) => message.id === messageId);
+  res.send(filterMessages);
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.post("/messages", (req, res) => {
+  const newMessage = req.body;
+  count++;
+  const id = { id: count };
+  messages.push(Object.assign(id, newMessage));
+  res.send("Your message was added successfully!");
+});
+
+app.delete("/messages/:messageId", (req, res) => {
+  const messageId = Number(req.params.messageId);
+  messages = messages.filter((message) => message.id !== messageId);
+  res.status(204).send();
 });
 
 app.listen(process.env.PORT, () => {
