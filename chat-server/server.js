ed98a0efc3d3fb8c26dcb5a029df931a1c6c974c
karimi <behrouzkarimi1993@gmail.com>
2024-03-26 14:58:49 +0000

    level-1 done, post, delete,and get

--- chat-server/server.js
+++ chat-server/server.js
@@ -6,6 +6,8 @@ import { fileURLToPath } from "url";
 
 const app = express();
 
+app.use(express.json());
+
 app.use(cors());
 
 // Get __dirname in ES module
@@ -23,6 +25,44 @@ const messages = [welcomeMessage];
 
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/index.html");
+});
+
+const usersChat = [];
+// const jsonCreator=(msg)={
+//   const message={
+//     id:,
+//     messages:msg,
+//     from:'Ali'
+//   }
+// }
+
+app.post("/messages", (req, res) => {
+  const chatData = req.body;
+  usersChat.push(chatData);
+  res.send("Chat added !");
+});
+
+app.get("/messages", (req, res) => {
+  res.send(usersChat);
+});
+
+const findMessageById = (arr, msgId) => {
+  return arr.filter((obj) => obj.id === Number(msgId));
+};
+
+app.get("/messages/:id", (req, res) => {
+  const msgId = req.params.id;
+  console.log(msgId);
+  const findedMsg = findMessageById(usersChat, msgId);
+  res.send(findedMsg);
+});
+
+app.delete("/messages/:id", (req, res) => {
+  const msgId = req.params.id;
+  const findedMsg = findMessageById(usersChat, msgId);
+  const index = usersChat.indexOf(findedMsg[0]);
+  usersChat.splice(index, 1);
+  res.send("Message deleted !");
 });
 
 app.listen(process.env.PORT, () => {
