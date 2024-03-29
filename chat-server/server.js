276e38e81da50be046a1639c02f6f54af902b808
bahador <noor.bahadory786@gmail.com>
2024-03-29 12:57:03 +0000

    created route find by id

--- chat-server/server.js
+++ chat-server/server.js
@@ -5,18 +5,19 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
-
 app.use(express.json());
 
 // Get __dirname in ES module
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const messages = {
-  id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
-};
-
+const messages = [
+  {
+    id: 0,
+    from: "Bart",
+    text: "Welcome to CYF chat system!",
+  },
+];
+let count = 0;
 //This array is our "data store".
 //We will start with one message in the array.
 //const messages = [welcomeMessage];
@@ -26,45 +27,28 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-//new server hep kita
+// get a new messages route
 app.get("/messages", (req, res) => {
   res.json(messages);
 });
 
-//marcus post code
+//post server
 app.use(express.urlencoded({ extended: true }));
 app.post("/messages", (req, res) => {
+  console.log("you are hitting post route!");
   const newMessage = req.body;
   if (newMessage.from === "" || newMessage.text === "") {
-    res.status(400);
+    res.status(400).json();
+    console.log("status 400");
   } else {
     count++;
     const id = { id: count };
     messages.push(Object.assign(id, newMessage));
+    console.log("your message was added successfully");
     res.send("Your message was added successfully!");
     console.log(messages);
   }
 });
-
-// Add a new message
-app.post("/messages", (req, res) => {
-  console.log("you are hitting post server !");
-  const myMessages = req.body;
-  messages.push(myMessages);
-  res.status(201).send({ newMovies: myMessages });
-});
-
-// app.post("/messages", (req, res) => {
-//   console.log("you are hitting post server !");
-//   const { from, text } = req.body;
-//   const newMessage = {
-//     id: messages.length,
-//     from,
-//     text,
-//   };
-//   messages.push(newMessage);
-//   res.json(newMessage);
-// });
 
 // Find message by ID
 app.get("/messages/:id", (req, res) => {
@@ -86,25 +70,22 @@ app.delete("/messages/:id", (req, res) => {
 });
 
 // Edit message by ID
-app.put("/messages/:id", (req, res) => {
-  console.log("you are hitting dynamic edit server!");
-  const idShow = req.params.id * 1;
-  const messageIndex = messages.findIndex((ele) => ele.id === idShow);
-  if (messageIndex !== -1) {
-    const { from, text } = req.body;
-    const updatedMessage = {
-      id: idShow,
-      from,
-      text,
-    };
-    messages[messageIndex] = updatedMessage;
-    res.json(updatedMessage);
-  }
-});
-
-// app.listen(PORT, () => {
-//   console.log(`Listening on PORT http://${HOST}:${PORT}`);
+// app.put("/messages/:id", (req, res) => {
+//   console.log("you are hitting dynamic edit server!");
+//   const idShow = req.params.id * 1;
+//   const messageIndex = messages.findIndex((ele) => ele.id === idShow);
+//   if (messageIndex !== -1) {
+//     const { from, text } = req.body;
+//     const updatedMessage = {
+//       id: idShow,
+//       from,
+//       text,
+//     };
+//     messages[messageIndex] = updatedMessage;
+//     res.json(updatedMessage);
+//   }
 // });
+
 app.listen(process.env.PORT, () => {
   console.log(`listening on PORT ${process.env.PORT}...`);
 });
