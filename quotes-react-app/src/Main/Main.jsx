7b6880a056aeb07315943d004f1d2b0a199e75f4
Pedro Ricciardi <riccip@outlook.com>
2024-04-09 09:11:17 +0100

    quotes fetch done

--- quotes-react-app/src/Main/Main.jsx
+++ quotes-react-app/src/Main/Main.jsx
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 export const Main = () => {
+  const [quotes, setQuotes] = useState([])
+
+  const quotesApi = "https://type.fit/api/quotes";
+
+  const fetchQuotes = async (quotesSource) => {
+    const response = await fetch(quotesSource);
+    const data = await response.json();
+    setQuotes(data);
+  }
+
+  fetchQuotes(quotesApi);
+
   return (
     <main>
-      <h2>Quotes</h2>
+      <h2>Random Quote</h2>
+      <p>Quote</p>
+      <button>New Quote!</button>
     </main>
   )
 }
