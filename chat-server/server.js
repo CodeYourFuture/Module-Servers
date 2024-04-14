65b275a09a4cfc54fd1324a40506beb03eecc6b0
= <adniyayousaf92@gmail.com>
2024-04-14 21:58:36 +0100

    updated the chat server where data is fetch from file system

--- chat-server/server.js
+++ chat-server/server.js
@@ -3,29 +3,23 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
+import { promises as fs } from 'fs';
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
-
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const welcomeMessage = {
-  "id": "0",
-  from: "Adniya",
-  text: "Welcome to Adniya chat system!",
-  time:"15:34:22"
-};
-
-const messagesArray = [welcomeMessage];
 
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/index.html");
 });
 
 app.get("/messages", (req, res) => {
-  res.send(messagesArray);
+  fs.readFile("messsages.json", "utf-8").then((messagesArray) => {
+    console.log(JSON.parse(messagesArray));
+    res.send(JSON.parse(messagesArray));
+  })
 });
 
 app.get("/messages/:messageId", (req, res) => {
@@ -37,9 +31,15 @@ app.get("/messages/:messageId", (req, res) => {
 app.post("/messages", function (req, res) {
   const newMessage = req.body;
   messagesArray.push(newMessage);
-  res.send(newMessage );
+  res.send(newMessage);
 });
 
+app.put("/messages/:id", function (req, res) {
+  const newMsg = { ...req.body, ...req.params };
+  const msgIndex = messagesArray.findIndex((msg) => msg.id === req.params.id);
+  console.log("New message updated", newMsg);
+  console.log("index of the message", msgIndex);
+});
 
 app.delete("/messages/:messageId", function (req, res) {
   const Id = req.params.messageId;
