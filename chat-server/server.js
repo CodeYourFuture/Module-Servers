93a0c1b417801219b18b07e31443c58a575b51f1
Sabella-8 <gsabella549@gmail.com>
2024-04-15 10:29:55 +0100

    chat

--- chat-server/server.js
+++ chat-server/server.js
@@ -5,7 +5,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
-
+app.use(express.json());
 app.use(cors());
 
 // Get __dirname in ES module
@@ -23,6 +23,16 @@ const messages = [welcomeMessage];
 
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/index.html");
+});
+
+app.get("/messages", (request, response) => {
+  response.json(messages);
+});
+app.post("/messages", (req, res) => {
+  const bod = req.body;
+  messages.push(bod);
+  console.log(bod);
+  res.json(messages);
 });
 
 app.listen(process.env.PORT, () => {
