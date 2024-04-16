610076182791d8bdba82e224cc841b82acfbbc5f
Pedro Ricciardi <riccip@outlook.com>
2024-04-16 08:18:49 +0100

    express.json added

--- chat-server/server.js
+++ chat-server/server.js
@@ -7,6 +7,7 @@ import { fileURLToPath } from "url";
 const app = express();
 
 app.use(cors());
+app.use(express.json())
 
 // Get __dirname in ES module
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
