65273cc99f327fc7a2fe85871c85c9944adcdbe3
karimi <behrouzkarimi1993@gmail.com>
2024-04-16 13:13:18 +0100

    name endpoint to list to get a list by name

--- mailing-list-api/server.js
+++ mailing-list-api/server.js
@@ -21,3 +21,23 @@ app.get("/list", (req, res) => {
     res.status(200).send([]);
   }
 });
+
+app.get("/list/:name", (req, res) => {
+  const endpointName = req.params.name;
+
+  const list = new Map(Object.entries(data));
+  // const listOfKeys = Array.from(list.keys());
+
+  const emailsOfMemebers = list.get(endpointName);
+  console.log(emailsOfMemebers);
+
+  if (emailsOfMemebers) {
+    const responseBody = {
+      name: endpointName,
+      members: emailsOfMemebers,
+    };
+    res.status(200).send(responseBody);
+  } else {
+    res.status(404).send("404 , Data not found!");
+  }
+});
