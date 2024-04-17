059f32e07a7fc0d87eda58550779bdd226f1b2a6
RbAvci <rb.avci@gmail.com>
2024-04-17 22:10:11 +0100

    add app.get for  /quotes

--- quote-server/server.js
+++ quote-server/server.js
@@ -17,14 +17,17 @@ app.get("/", (request, response) => {
 
 //START OF YOUR CODE...
 
+app.get("/quotes", (req, res) => {
+  res.json(quotes);
+});
+
 //...END OF YOUR CODE
 
 //You can use this function to pick one element at random from a given array
 //example: pickFromArray([1,2,3,4]), or
 //example: pickFromArray(myContactsArray)
 //
-const pickFromArray = (arrayofQuotes) =>
-  arrayofQuotes[Math.floor(Math.random() * arrayofQuotes.length)];
+const pickFromArray = (arrayofQuotes) => arrayofQuotes[Math.floor(Math.random() * arrayofQuotes.length)];
 
 //Start our server so that it listens for HTTP requests!
 const listener = app.listen(3001, () => {
