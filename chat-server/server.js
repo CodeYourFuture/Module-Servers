dd02cce115a9dd65280e08a781ba10d9c2bb032f
RbAvci <rb.avci@gmail.com>
2024-04-18 22:06:27 +0100

    added post message

--- chat-server/server.js
+++ chat-server/server.js
@@ -5,30 +5,31 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
-
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(cors());
 
 // Get __dirname in ES module
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const welcomeMessage = [
-  {
-    id: 0,
-    from: "Bart",
-    text: "Welcome to CYF chat system!",
-  },
-  {
-    id: 1,
-    from: "test",
-    text: "test",
-  },
-];
+const welcomeMessage = {
+  id: 0,
+  from: "Bart",
+  text: "Welcome to CYF chat system!",
+};
+
+const welcomeMessage2 = {
+  id: 1,
+  from: "test",
+  text: "test",
+};
 
 let lastId = 1;
 
 //This array is our "data store".
 //We will start with one message in the array.
-const messages = [welcomeMessage];
+
+const messages = [welcomeMessage, welcomeMessage2];
 
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/index.html");
@@ -36,16 +37,32 @@ app.get("/", (request, response) => {
 
 // Get all messages
 app.get("/messages", (req, res) => {
-  res.send(welcomeMessage);
+  res.send(messages);
 });
 
 // GET a specific message by id
 app.get("/messages/:id", (req, res) => {
-  const message = welcomeMessage.find((p) => p.id === parseInt(req.params.id));
-  if (!message) return res.status(404).json({ message: "Post not found" });
+  const message = messages.find((p) => p.id === parseInt(req.params.id));
+  if (!message) return res.status(404).json({ message: "Message not found" });
   res.json(message);
+});
+
+// POST a new message
+
+app.post("/messages", (req, res) => {
+  const newId = (lastId += 1);
+  console.log(req);
+  const message = {
+    id: newId,
+    from: req.body.from,
+    text: req.body.text,
+  };
+  lastId = newId;
+  messages.push(message);
+  res.status(201).json(message);
 });
 
 app.listen(process.env.PORT, () => {
   console.log(`listening on PORT ${process.env.PORT}...`);
 });
+
