807ec04e1c38d3751314b8bbf0d2f187018f6372
RbAvci <rb.avci@gmail.com>
2024-04-18 11:41:25 +0100

    add get/ messages route

--- chat-server/server.js
+++ chat-server/server.js
@@ -1,5 +1,5 @@
 process.env.PORT = process.env.PORT || 9090;
-import express from "express";
+import express, { json } from "express";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -11,11 +11,14 @@ app.use(cors());
 // Get __dirname in ES module
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const welcomeMessage = {
+const welcomeMessage =[{
   id: 0,
   from: "Bart",
   text: "Welcome to CYF chat system!",
-};
+},
+];
+
+let lastId= 0,
 
 //This array is our "data store".
 //We will start with one message in the array.
@@ -23,6 +26,18 @@ const messages = [welcomeMessage];
 
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/index.html");
+});
+
+// Get all messages
+app.get("/messages", (req, res) => {
+  res.send(welcomeMessage);
+});
+
+// GET a specific message by id
+app.get("/messages/:id", (req, res) => {
+  const message = welcomeMessage.find((p) => p.id === parseInt(req.params.id));
+  if (!post) return res.status(404).json({ message: "Post not found" });
+  res.json(post);
 });
 
 app.listen(process.env.PORT, () => {
