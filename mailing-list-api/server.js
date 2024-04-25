75d335dc9c0e2df9cce7533db5bef2ed605a26f8
Sabella-8 <gsabella549@gmail.com>
2024-04-25 14:57:37 +0100

    mailing server

--- mailing-list-api/server.js
+++ mailing-list-api/server.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const data = require("./mailing-lists");
 const app = express();
+app.use(express.json());
 const lists = new Map(Object.entries(data));
 app.get("/lists", (req, res) => {
   const listsArray = Array.from(lists.keys());
-  if (lists) {
+  if (listsArray.length > 0) {
     res.status(200).send(listsArray);
   } else {
     res.status(200).send({});
@@ -29,7 +30,21 @@ app.delete("/lists/:name", (req, res) => {
     res.status(404).send("list not found");
   }
 });
-
+app.put("/lists/:name", (req, res) => {
+  const name = req.params.name.toLowerCase();
+  if (lists.has(name)) {
+    const putListBody = req.body;
+    lists.set(name, putListBody);
+    const updatedList = Array.from(lists.keys());
+    res.status(200).send(updatedList);
+  } else {
+    const putListBody = req.body;
+    console.log(putListBody);
+    lists.set(name, putListBody);
+    const updatedList = Array.from(lists.keys());
+    res.status(201).send(updatedList);
+  }
+});
 app.listen(3000, () => {
   console.log("Server is listen to 3000");
 });
