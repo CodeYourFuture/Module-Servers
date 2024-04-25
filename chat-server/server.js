ba46b564848783d4124002874d99453a23665f76
Your Name <you@example.com>
2024-04-26 00:04:43 +0100

    done get and post

--- chat-server/server.js
+++ chat-server/server.js
@@ -5,7 +5,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
-
+app.use(express.json());
 app.use(cors());
 
 // Get __dirname in ES module
@@ -24,6 +24,30 @@ const messages = [welcomeMessage];
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/index.html");
 });
+
+//read all messages
+app.get("/messages",(req, res) => {
+  res.send(messages);
+})
+
+//get messages by id
+app.get("/messages/:id", (req,res) => {
+  const messageID = Number(req.params.id);
+  const message = messages.find((message) => message.id === messageID);
+  res.send(message);
+})
+
+//create new messages
+app.post("/messages", (req,res) => {
+  const newMessage = req.body;
+  messages.push(newMessage);
+  res.send(newMessage)
+})
+
+//delete messages by id
+app.delete("/messages/id", (req,res) => {
+
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`listening on PORT ${process.env.PORT}...`);
