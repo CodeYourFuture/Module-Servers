ebdefe7282237d715eb31eece392247146dc7f28
areebsattar <areebsattar1@gmail.com>
2024-04-25 23:35:29 +0100

    Completed write and read all messages and messages with id route

--- chat-server/server.js
+++ chat-server/server.js
@@ -6,6 +6,7 @@ import { fileURLToPath } from "url";
 
 const app = express();
 
+app.use(express.json());
 app.use(cors());
 
 // Get __dirname in ES module
@@ -23,6 +24,23 @@ const messages = [welcomeMessage];
 
 app.get("/", (request, response) => {
   response.sendFile(__dirname + "/index.html");
+});
+
+app.post("/messages", (req, res) => {
+  const newMessage = req.body;
+  messages.push(newMessage);
+  res.send("Message added");
+});
+
+app.get("/messages", (req, res) => {
+  res.send(messages);
+});
+
+app.get("/messages/:id", (req, res) => {
+  const messageId = parseInt(req.params.id);
+  const message = messages.find((obj) =>  obj.id === messageId );
+  if (!message) return res.status(404).json({ message: "Message does not exist" });
+  res.json(message);
 });
 
 app.listen(process.env.PORT, () => {
