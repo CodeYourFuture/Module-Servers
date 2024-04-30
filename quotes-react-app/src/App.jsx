8eaaa9f873ff586da06fc3049a4a33a4cf48f722
Pedro Ricciardi <riccip@outlook.com>
2024-04-30 09:59:53 +0100

    app export changed to named export

--- quotes-react-app/src/App.jsx
+++ quotes-react-app/src/App.jsx
@@ -3,7 +3,7 @@ import { Footer } from "./Footer/Footer";
 import { Header } from "./Header/Header";
 import { Main } from "./Main/Main";
 
-function App() {
+export function App() {
   return (
     <>
       <Header />
@@ -12,5 +12,3 @@ function App() {
     </>
   );
 }
-
-export default App;
